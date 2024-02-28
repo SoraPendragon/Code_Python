176f14864a8def632ee93222be22b0c95db9333f
SoraPendragon <denismaximedu13@gmail.com>
2024-02-28 23:39:47 +0100

    First Fight done
    First fight with an octopus

--- terminal_game.py
+++ terminal_game.py
@@ -13,20 +13,80 @@ print ("A pleasure to meet you", reader_name, "! What should we do today?")
 print ("Shall we go outside? See if the sea is happy today?(1) \nOr should we stay in bed?(2) ")
 choice = int(input("What do you want to do? "))
 
-if reader_hp <= 0:
-    print ("You have", reader_hp, "Try left")
-    print ("Game Over...")
-    
 if choice == 1:
     print ("-------------------------- A Day on a Boat --------------------------")
-    print ("---------------- Or: Why did I choose this project?  ----------------")
-    print ("The sea look beautiful, you can see a miles away! \n The ship need some love, maybe try to wash the deck?(1)\n Oh I know! We can go fish our lunch!(2) ")
-    print ("I have", user_hp,"HP")
-    print ("You can try ", reader_hp,"times")
+    print ("---------------- Or: Why did I choose this project?  ----------------")             #J'ai perdu 15 Min à faire ce truc, on peut m'expliquer pourquoi?
+    print ("The sea look beautiful, you can see a miles away! \n Oh no! An Octopus is attacking the ship! \n Quick! Grab your tool, you need to defend the ship! ")
+    print ("------------------------------- _____ -------------------------------")
+    print ("------------------------------ /     \ ------------------------------")
+    print ("------------------------------ I 0 0 I ------------------------------")
+    print ("-------------------------- ___ \  w  /  ___ -------------------------")
+    print ("------------------------- /   \ -- --  /   \ ------------------------")
+    print ("------------------- __    I   /  /I\   \   I   __ -------------------")
+    print ("------------------ /  \   \     / I \     /   /  \ ------------------")
+    print ("------------------ \_______\___/  I  \___/_______/ ------------------")
+    print ("------------------ / __     __    I    __     __ \ ------------------")
+    print ("----------------- I /  \   /  \  / \  /  \   /  \ I -----------------")
+    print ("------------------ \___/   \____/   \____/   \___/ ------------------")
+    print ("---------------------------------------------------------------------")
+    print ("You have", reader_hp,"lifes")
 elif choice == 2 :
     print("Oh no, your bed sheet is trying to swallow you whole!")
-    reader_hp = 0
+    user_hp = 0
     print (reader_name,"... I'm sorry... We can't do it")
-    reader_hp = reader_hp - 1
+    reader_hp -= 1
 else:
     choice = int(input("Please enter a valid choice to start your adventure: "))
+
+#Choix 1
+monster_hp = 5
+mon_move = {
+    "Tentacle": 1,
+    "Head" : 2,
+    "Double Tentacle" : 4,
+}
+
+user_move = {
+    "It's a Critical hit!" : 5,
+    "You land a Normal Hit" : 2,
+    "You Missed..." : 0,
+}
+
+while user_hp > 0 and monster_hp > 0:
+    print ("Your turn!")
+    user_attack = int(input("Attack(1) or Defend(2)! It's your choice: "))
+    monster_attack = random.choice(list(mon_move))         # Choose the Attack for the Monster : Tentacule (1 dmg), Head (2 dmg), Double Tentacule (4 Dmg)
+    
+    if user_attack == 1 :
+        print ("You punch the octopus in the face")
+        user_damage = random.choice(list(user_move))
+        monster_hp -= user_move[user_damage]
+        print (user_damage)
+        print ("the Monster use", monster_attack)
+        print ("It deal",mon_move[monster_attack],"damage")
+        user_hp = user_hp - mon_move[monster_attack]
+
+    elif user_attack == 2:
+        print ("You defend yourself")
+        user_hp = user_hp - (mon_move[monster_attack] // 2)
+    
+    else:
+        choice = int(input("Please enter a valid action"))
+    
+    print ("the Monster use", monster_attack)
+    print ("You lose", mon_move[monster_attack],"HP")
+    print ("HP left", user_hp)
+    print ("The Monster has", monster_hp, "HP left")
+
+while True:
+    if user_hp >= 0 and monster_hp <= 0 :
+        print ("You win!")
+        print ("Time to eat your lunch!")
+        continue
+    else :
+        print ("You loose...")
+        reader_hp -= 1
+        break    
+
+print ("You had a good fight try to remember it for the rest of the day")
+print ("It's time to wash the ")
